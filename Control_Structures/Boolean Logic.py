a798efe31522b5a5fb265833b34d0209d62e1f2a
00Bren00 <ringelb8@students.rowan.edu>
2017-05-11 23:42:25 -0400

    boolean logic examples and notes were added

---
+++ Control_Structures/Boolean Logic.py
@@ -0,0 +1,7 @@
+print (True and False) # and statements use 'and'
+print (False or True)   # or statements use 'or'
+print (not True)        #not operator is 'not'
+
+# see https://www.sololearn.com/Play/Python/
+# for more info on different operators and
+#operator precedence. 
