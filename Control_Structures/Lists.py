b8df8825760441d748e46cbf09d77a5d8c759cc2
00Bren00 <ringelb8@students.rowan.edu>
2017-05-12 00:08:11 -0400

    More notes on list append insert len index max min count remove reverse

--- Control_Structures/Lists.py
+++ Control_Structures/Lists.py
@@ -24,3 +24,31 @@ print(4 not in nums)
 print(not 3 in nums)
 print(3 not in nums)
 #can combine not with in
+
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+#can append items to the end of a list
+
+nums = [1, 3, 5, 2, 4]
+print(len(nums))#5
+#len function gets the num of items in a list
+
+words = ["Python", "fun"]
+index = 1
+words.insert(index, "is")
+print(words)#['Python', 'is', 'fun']
+#insert adds items at a specific index
+
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+print(letters.index('r'))
+print(letters.index('p'))
+print(letters.index('z'))
+#The index method finds the first occurrence of a list item and returns its index.
+#If the item isn't in the list, it raises a ValueError.
+#There are a few more useful functions and methods for lists.
+#max(list): Returns the list item with the maximum value
+#min(list): Returns the list item with minimum value
+#list.count(obj): Returns a count of how many times an item occurs in a list
+#list.remove(obj): Removes an object from a list
+#list.reverse(): Reverses objects in a list
